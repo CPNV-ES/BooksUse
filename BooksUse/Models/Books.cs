c7bbfd90c4b3cc4ebcc266ffc0baaf1126fda88a
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-03-15 08:57:47 +0100

    Add display name in model

--- BooksUse/Models/Books.cs
+++ BooksUse/Models/Books.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace BooksUse.Models
 {
@@ -11,13 +13,20 @@ namespace BooksUse.Models
         }
 
         public int Id { get; set; }
+        [DisplayName("Nom")]
         public string Title { get; set; }
         public string Isbn { get; set; }
+        [DisplayName("Année")]
+        [DataType(DataType.Date)]
         public DateTime? Year { get; set; }
+        [DisplayName("Auteur(s)")]
         public string Author { get; set; }
+        [DisplayName("Stock")]
         public int? UnitsInStock { get; set; }
+        [DisplayName("Prix")]
         public decimal? Price { get; set; }
 
         public virtual ICollection<Requests> Requests { get; set; }
+
     }
 }
--- BooksUse/Models/Requests.cs
+++ BooksUse/Models/Requests.cs
@@ -1,17 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 
 namespace BooksUse.Models
 {
     public partial class Requests
     {
         public int Id { get; set; }
+        [DisplayName("Année")]
         public int ForYear { get; set; }
+        [DisplayName("Approuvé")]
         public int Approved { get; set; }
         public int FkUsers { get; set; }
         public int FkBooks { get; set; }
 
+        [DisplayName("Livre")]
         public virtual Books FkBooksNavigation { get; set; }
+        [DisplayName("Responsable")]
         public virtual Users FkUsersNavigation { get; set; }
     }
 }
