421b51f369dff6f795e549d3fee9752187cddf75
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-03-15 08:59:51 +0100

    Add config file

---
+++ BooksUse/config.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksUse
+{
+    public static class config
+    {
+        public static int intranetId = 12;
+        // IntranetId
+        //      12 => Xavier Carrel
+        //      24 => Pascal Benzonana
+        //      34 => Jean Dupont
+    }
+}
