fd01c3818d5465080bb2ad0410da4e4f9ccb5995
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-03-16 16:21:17 +0100

    Add start controller for different view for roles

---
+++ BooksUse/Controllers/StartController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BooksUse.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksUse.Controllers
+{
+    public class StartController : Controller
+    {
+        private readonly BooksUseContext _context;
+
+        public StartController(BooksUseContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index(BooksUseContext context)
+        {
+            //get current user
+            var currentUser = await _context.Users.FirstOrDefaultAsync(r => r.IntranetUserId == config.intranetId);
+
+            if(currentUser.FkRoles == 1)
+            {
+                return RedirectToAction("Index", "Requests");
+            }
+            else if(currentUser.FkRoles == 2)
+            {
+                return RedirectToAction("Index", "Books");
+            }
+            else
+            {
+                return NotFound();
+            }
+
+            
+        }
+    }
+}
