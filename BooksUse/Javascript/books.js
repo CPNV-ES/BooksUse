1cb48cf65cf5b6308cb1359f18d6a65c0ce4f6fc
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-03-19 17:19:31 +0100

    Add minify js

---
+++ BooksUse/Javascript/books.js
@@ -0,0 +1,37 @@
+﻿$(document).ready(function () {
+    $(document).on('click', '.submitBooksButton', function (event) {
+        event.preventDefault()
+        let error = false
+
+        $("#title").removeClass("is-invalid")
+        $("#isbn").removeClass("is-invalid")
+        $("#author").removeClass("is-invalid")
+        $("#stock").removeClass("is-invalid")
+        $("#price").removeClass("is-invalid")
+
+
+        if ($("#title").val() == "") {
+            $("#title").addClass("is-invalid")
+            error = true
+        }
+        if ($("#isbn").val() == "") {
+            $("#isbn").addClass("is-invalid")
+            error = true
+        }
+        if ($("#author").val() == "") {
+            $("#author").addClass("is-invalid")
+            error = true
+        }
+        if ($("#stock").val() == "") {
+            $("#stock").addClass("is-invalid")
+            error = true
+        }
+        if ($("#price").val() == "") {
+            $("#price").addClass("is-invalid")
+            error = true
+        }
+        if (!error) {
+            document.getElementById("form").submit();
+        }
+    })
+})
