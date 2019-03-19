8195f0bdb1d7f76d5ae49027b0bed7ab7989c485
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-03-19 15:04:00 +0100

    generate API requests controller

---
+++ BooksUse/Controllers/RequestsAPIController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BooksUse.Models;
+
+namespace BooksUse.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequestsAPIController : ControllerBase
+    {
+        private readonly BooksUseContext _context;
+
+        public RequestsAPIController(BooksUseContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/RequestsAPI
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Requests>>> GetRequests()
+        {
+            return await _context.Requests.ToListAsync();
+        }
+
+        // GET: api/RequestsAPI/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Requests>> GetRequests(int id)
+        {
+            var requests = await _context.Requests.FindAsync(id);
+
+            if (requests == null)
+            {
+                return NotFound();
+            }
+
+            return requests;
+        }
+
+        // PUT: api/RequestsAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRequests(int id, Requests requests)
+        {
+            if (id != requests.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(requests).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RequestsExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/RequestsAPI
+        [HttpPost]
+        public async Task<ActionResult<Requests>> PostRequests(Requests requests)
+        {
+            _context.Requests.Add(requests);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRequests", new { id = requests.Id }, requests);
+        }
+
+        // DELETE: api/RequestsAPI/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Requests>> DeleteRequests(int id)
+        {
+            var requests = await _context.Requests.FindAsync(id);
+            if (requests == null)
+            {
+                return NotFound();
+            }
+
+            _context.Requests.Remove(requests);
+            await _context.SaveChangesAsync();
+
+            return requests;
+        }
+
+        private bool RequestsExists(int id)
+        {
+            return _context.Requests.Any(e => e.Id == id);
+        }
+    }
+}
