872f353894bd24176f2d170cea691c8002694c81
Kevin Jordil <kevin.jordil@cpnv.ch>
2019-03-21 16:46:25 +0100

    Add new script for apibooks

---
+++ BooksUse/Javascript/userSelect.js
@@ -0,0 +1,23 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+
+    function setBooksSelect() {
+        $("#booksSelect").find('option').remove().end()
+        $.get('/api/BooksAPI/GetAvaibleBooks/' + $("#userSelect").val(), function (data) {
+            var avaibleBooks = ""
+            for (var i = 0; i < data.length; i++) {
+                avaibleBooks += "<option value='" + data[i].id + "'>" + data[i].title + "</option>";
+            }
+            $('#booksSelect').append(avaibleBooks);
+            $('#submitButton').prop('disabled', false)
+            if (data.length == 0) {
+                $('#submitButton').prop('disabled', true)
+            }
+        })
+    }
+
+    setBooksSelect()
+
+    $("#userSelect").change(function (event) {
+        setBooksSelect()
+    })
+})
